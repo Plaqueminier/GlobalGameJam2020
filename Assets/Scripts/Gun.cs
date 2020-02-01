86a7cbd4557cb06b114a50a71f5e05e42fd93706
CamilleWS <camille.villa@epitech.eu>
2020-02-01 13:49:42 +0100

    add laserRay

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -10,13 +10,40 @@ public class Gun : MonoBehaviour
 
     public Camera fpsCam;
     public GameObject impactEffect;
+    public float damageRate = 10f;
 
-    // Update is called once per frame
+    public LineRenderer lineRenderer;
+    bool release = true;
+
+    private float nextTimeToFire = 0f;
+    void Start()
+    {
+        lineRenderer.enabled = false;
+    }
+
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            lineRenderer.SetPosition(1, hit.point + hit.normal);
+        }
+        if (Input.GetButton("Fire1") && !release)
         {
             Shoot();
+        }
+        if (Input.GetButtonDown("Fire1") && release)
+        {
+            Shoot();
+            release = false;
+            //  sounds //
+            FindObjectOfType<AudioManager>().Play("test");
+
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            release = true;
+            lineRenderer.enabled = false;
         }
 
     }
@@ -24,14 +51,17 @@ public class Gun : MonoBehaviour
     void Shoot()
     {
         RaycastHit hit;
+        Target target;
+
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
             Debug.Log(hit.transform.name);
 
-            Target target = hit.transform.GetComponent<Target>();
-            if (target != null)
+            target = hit.transform.GetComponent<Target>();
+            if (target != null && Time.time >= nextTimeToFire)
             {
                 target.TakeDamage(damage);
+                nextTimeToFire = nextTimeToFire = Time.time + 2f / damageRate;
             }
             if (hit.rigidbody != null)
             {
@@ -41,11 +71,14 @@ public class Gun : MonoBehaviour
             GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
             Destroy(impactGO, 2f);
 
-            //  sounds //
-            FindObjectOfType<AudioManager>().Play("test");
+            // lineRenderer.SetPosition(0, transform.position);
+
+
+            lineRenderer.enabled = true;
 
 
         }
+
 
     }
 }
