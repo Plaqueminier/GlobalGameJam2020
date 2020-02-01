0086051a5f8e9f173d1671b8c8199166df4a7e1d
CamilleWS <camille.villa@epitech.eu>
2020-02-02 00:52:34 +0100

    add life tmp

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerManager : MonoBehaviour
 {
@@ -9,10 +10,18 @@ public class PlayerManager : MonoBehaviour
     public bool inputPaused;
     public static PlayerManager instance;
 
+
+    public Image[] hearts;
+    public int numOfHearts;
+
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
     void Awake()
     {
         inputPaused = false;
-        if (instance != null) {
+        if (instance != null)
+        {
             Debug.Log("Warning, multiple players");
             return;
         }
@@ -27,13 +36,37 @@ public class PlayerManager : MonoBehaviour
 
     void Update()
     {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < health)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+            if (i < numOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
 
+        }
     }
 
     public void TakeDamage(float damage)
     {
+        if (!FindObjectOfType<AudioManager>().IsPlaying("damage"))
+        {
+            FindObjectOfType<AudioManager>().Play("damage");
+        }
         health -= damage;
-        if (health <= 0) {
+        if (health <= 0)
+        {
             // Die
         }
     }
