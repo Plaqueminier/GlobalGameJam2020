3cbaefd5ed4b903b73aaa1f4dcbf376c73967d95
CamilleWS <camille.villa@epitech.eu>
2020-02-02 14:13:49 +0100

    add particule to death mob

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     public float health = 50f;
+    public GameObject impactEffect;
+    //public GameObject BloodEffect;
 
     public void TakeDamage(float amount)
     {
         health -= amount;
 
+
+        // GameObject impactGO2 = Instantiate(BloodEffect, transform.position, Quaternion.identity);
+        // Destroy(impactGO2, 2f);
         if (health <= 0f)
         {
             Die();
+
+
+            GameObject impactGO = Instantiate(impactEffect, transform.position, Quaternion.identity);
+
+            Destroy(impactGO, 2f);
         }
 
     }
